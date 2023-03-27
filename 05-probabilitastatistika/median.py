a7311685f38df72c4091e7c43bb7f38bc15f1faf
ezerinz <edwinnnzx@gmail.com>
2023-03-27 19:54:36 +0800

    apdet rumus median jumlah ganjil

--- 05-probabilitastatistika/median.py
+++ 05-probabilitastatistika/median.py
@@ -3,24 +3,21 @@
 data = [0, 2, 2, 1, 3, 7, 1]
 
 jumlah_data = len(data)
-genap = jumlah_data % 2 == 0
 
 for i in range(jumlah_data):
     for j in range(0, jumlah_data - i - 1):
         if data[j] > data[j + 1]:
-            temp = data[j]
-            data[j] = data[j+1]
-            data[j+1] = temp
+            data[j], data[j+1] = data[j+1], data[j]
 
 
 print(f"Data setelah diurut: ", data)
-if genap:
+if jumlah_data % 2 == 0:
     index_satu = int(jumlah_data / 2)
     index_dua = int(jumlah_data / 2 + 1)
 
     median = (data[index_satu-1] + data[index_dua-1]) / 2
 else:
-    index_median = int(jumlah_data / 2)
-    median = data[index_median]
+    index_median = int((jumlah_data + 1) / 2)
+    median = data[index_median-1]
 
 print(f"Median: {median}")
