999522e61ef80b2629edf2f90be320c63928a45c
ezerinz <edwinnnzx@gmail.com>
2023-03-28 22:18:16 +0800

    better way to count freq

--- 05-probabilitastatistika/modus.py
+++ 05-probabilitastatistika/modus.py
@@ -4,13 +4,13 @@ data = [0, 2, 2, 1, 3, 7, 1]
 frekuensi = {}
 
 for i in data:
-    frek = 0
-    for j in data:
-        if i == j:
-            frek += 1
-            frekuensi[i] = frek
+    if i in frekuensi:
+        frekuensi[i] += 1
+    else:
+        frekuensi[i] = 1
 
 maks = list(frekuensi.keys())[0]
+
 for i in frekuensi:
     if frekuensi[maks] <= frekuensi[i]:
         maks = i
@@ -20,12 +20,6 @@ for j in frekuensi:
     if frekuensi[maks] != frekuensi[j]:
         semua_sama = False
 
-modus = []
-if not semua_sama:
-    for k in frekuensi:
-        if frekuensi[k] == frekuensi[maks]:
-            modus.append(k)
-
 # Menampilkan
 print(f"Data: {data}")
 print("Frekuensi: ")
@@ -33,8 +27,13 @@ for l in frekuensi:
     print(f"{l} = {frekuensi[l]}")
 
 print()
-if modus:
+if not semua_sama:
     print("Modus: ", end="")
+    modus = []
+    for k in frekuensi:
+        if frekuensi[k] == frekuensi[maks]:
+            modus.append(k)
+
     print(*modus, sep=", ")
 else:
     print("Modus tidak ada")
